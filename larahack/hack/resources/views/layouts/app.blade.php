ae09bc384b1f61da307289c1ff9261dbeeb79a61
tymo.dekock <tymo.dekock@telenet.be>
2018-12-18 11:28:46 +0100

    added morefunc

--- larahack/hack/resources/views/layouts/app.blade.php
+++ larahack/hack/resources/views/layouts/app.blade.php
@@ -2,12 +2,12 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" type="image/x-icon" href="https://mijnrooster.kdg.be/assets/e0b1c8c08/images/favicon.ico">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Forms | Karel de Grote University College</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
