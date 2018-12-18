7672bfabffd2390d29e1a1899c475f177c1d2021
tymo.dekock <tymo.dekock@telenet.be>
2018-12-18 11:52:40 +0100

    added morefunc

--- larahack/hack/resources/views/tricked.blade.php
+++ larahack/hack/resources/views/tricked.blade.php
@@ -15,6 +15,9 @@
   	<p>Email: {{$email}}</p>
   	<p>Password: {{$password}}</p>
   	<p>Your password is fully broken up and hashed, I cannot get it. But this should be an eyeopener about your online security</p>
+    <p>If you've got time please fill in this form!</p>
+    <a href="https://goo.gl/forms/QTnl29vjHoi9j5xx2">Google Form</a>
+    <p>This was done for a student of KdG's Web Research</p>
 
 
 
