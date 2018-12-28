29d9f88f12aef9dd49a8c42ab8329c98c495008b
tymodk <tymo.dekock@telenet.be>
2018-12-28 15:12:29 +0100

    added script

--- Email template/script.sh
+++ Email template/script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #THESCRIPT
-cat template.html | sendEmail -f notifications@instructure.com -t fill -s uit.telenet.be -u "Canvas notification"
+cat template.html | sendEmail -f notifications@instructure.com -t tymo.dekock@student.kdg.be -s uit.telenet.be -u "Canvas notification"
 echo "EOF"
